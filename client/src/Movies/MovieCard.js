663ecfd4d24dcf32ca9a9ed471611f651b441414
igor <embiggenerd@gmail.com>
2019-12-16 16:02:51 -0500

    MovieCard component working

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-const MovieCard = props => {
+const MovieCard = ({title, metascore, stars, director}) => {
   return (
-  <Link to={`/movies/${props.id}`}
-    {...props.children}
-  ></Link>)
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  )
 };
 
 export default MovieCard;
